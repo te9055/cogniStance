20e3b60e76ff0f3a12a0ee3451a874abe3cbcc99
Tom Edwards <edwardstj1@cardiff.ac.uk>
2024-06-19 10:10:03 +0100

    Work towards USAS

--- func/usas/usas.py
+++ func/usas/usas.py
@@ -1,7 +1,6 @@
 import spacy
 
 
-
 # Perform USAS on Text
 def run_usas_on_text(page):
     # We exclude the following components as we do not need them.
@@ -17,7 +16,5 @@ def run_usas_on_text(page):
     for token in output_doc:
         print(f'{token.text}\t{token.pos_}\t{token._.pymusas_tags}')
 
-
-
-    result = {'output': "Hello USAS",'message': 'Done', 'code': 'SUCCESS'}
+    result = {'output': "Hello USAS", 'message': 'Done', 'code': 'SUCCESS'}
     return result
